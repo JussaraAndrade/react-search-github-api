2f4bfcf95d3eaee76f68818afbefae824516d89c
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-01-11 13:38:58 -0300

    Create front-web api initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { useState } from "react";
+import "./App.css";
+import GithubImage from "./github.png";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then((response) => response.json())
+      .then((userResponse) => setUserData(userResponse));
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={search}
+              onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        {!userData && (
+          <img
+            src={GithubImage}
+            className="responsive rounded-circle "
+            alt=""
+            height="200px"
+          />
+        )}
+
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className="responsive rounded-circle "
+              alt=""
+              height="200px"
+            />
+            <h1 className="pt-5">
+              <a href="https://github.com/JussaraAndrade" target="_new">
+                {userData.login}
+              </a>
+            </h1>
+
+            <h3>{userData.location}</h3>
+            <p>
+              <a href={userData.blog} target="_new" className="text-info"></a>
+              {userData.blog}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
